c24b257bfecbc2808d0a78b3632b25e7b13c516b
Asiah Clark <clarkaa0@gmail.com>
2024-02-28 20:00:51 -0600

    Added Normal class with same code as in normal_tick method. normal_tick method now calls a normal object with tick method inside Normal class.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,11 +21,8 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
   def brie_tick
     @days_remaining -= 1
@@ -44,5 +41,20 @@ class GildedRose
     @quality += 1
     @quality += 1 if @days_remaining < 10
     @quality += 1 if days_remaining < 5
+  end
+end
+
+class Normal
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+  def tick
+    @days_remaining -= 1
+    return if @quality == 0
+
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
   end
 end
