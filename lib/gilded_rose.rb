751303fd78e3f74186b9f1e6e5659d4c686eac30
Asiah Clark <clarkaa0@gmail.com>
2024-02-28 20:11:42 -0600

    Added Sulfuras class. tick method in said class is empty like sulfuras_tick was. sulfuras_tick initializes a sulfuras object.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -37,6 +37,8 @@ class GildedRose
     @days_remaining
   end
   def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
   end
   def backstage_tick
     @days_remaining -= 1
@@ -76,5 +78,15 @@ class Brie
 
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
+end
+
+class Sulfuras
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+  def tick
   end
 end
